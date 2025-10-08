151a8f8dfb5edb30a86fa16ab9ffa597863357e5
Hayden Howle <hayden.howle@teambespin.us>
2025-10-08 09:50:18 -0500

    testing specifying root cert

--- backend/wedding.go
+++ backend/wedding.go
@@ -6,9 +6,6 @@ import (
 	"log"
 	"os"
 
-	"crypto/tls"
-	"crypto/x509"
-
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 
@@ -38,21 +35,22 @@ func main() {
 	cfg.ConnConfig.User = os.Getenv("DB_USER")
 	cfg.ConnConfig.Password = os.Getenv("DB_PASSWORD")
 	cfg.ConnConfig.TLSConfig = nil
-	// cfg.ConnConfig.Config.RuntimeParams["sslmode"] = os.Getenv("DB_SSLMODE")
+	cfg.ConnConfig.Config.RuntimeParams["sslmode"] = os.Getenv("DB_SSLMODE")
+	cfg.ConnConfig.Config.RuntimeParams["sslrootcert"] = os.Getenv("DB_ROOT_CERT")
 	// Use system cert pool (ensure ca-certificates are installed)
-	roots, err := x509.SystemCertPool()
-	if err != nil || roots == nil {
-		roots = x509.NewCertPool()
-	}
+	// roots, err := x509.SystemCertPool()
+	// if err != nil || roots == nil {
+	// 	roots = x509.NewCertPool()
+	// }
 
-	cfg.ConnConfig.TLSConfig = &tls.Config{
-		ServerName: os.Getenv("DB_HOST"),
-		RootCAs:    roots,
-		MinVersion: tls.VersionTLS12,
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
+	// cfg.ConnConfig.TLSConfig = &tls.Config{
+	// 	ServerName: os.Getenv("DB_HOST"),
+	// 	RootCAs:    roots,
+	// 	MinVersion: tls.VersionTLS12,
+	// }
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	router := gin.Default()
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 

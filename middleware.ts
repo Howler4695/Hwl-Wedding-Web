fae44fbf1cbc5d0f133c4d1c15aab01cc5238a26
Hayden Howle <hayden.howle@teambespin.us>
2025-11-18 12:59:48 -0600

    fixed public pictures getting caught in middleware

--- middleware.ts
+++ middleware.ts
@@ -27,6 +27,6 @@ export default auth((req) => {
 
 export const config = {
   matcher: [
-    "/((?!api/auth/|auth/signin|auth/signout|auth/reauth|_next/static|_next/image|favicon.ico|__nextjs_font|images/|magnolia_no_stem.svg).*)",
+    "/((?!api/auth/|auth/signin|auth/signout|auth/reauth|_next/static|_next/image|favicon.ico|__nextjs_font|images/|magnolia_no_stem.svg|front_pic_0.jpg|front_pic_1.jpg|front_pic_2.jpg|front_pic_3.jpg).*)",
   ],
 };

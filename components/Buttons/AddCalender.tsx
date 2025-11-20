fd83f4e8828098673ae2fe7f023aca44d950a9d5
Hayden Howle <hayden.howle@teambespin.us>
2025-11-19 19:24:55 -0600

    fixed add calender button

--- components/Buttons/AddCalender.tsx
+++ components/Buttons/AddCalender.tsx
@@ -3,7 +3,7 @@
 export default function AddCalenderButton() {
   return (
     <a
-      href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NTdhOWZndnJvZDg4NTQ0NGJkOGc3b3FhajcgaG93bGVyNDY5NUBt&tmsrc=howler4695%40gmail.com"
+      href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Howle%20Wedding&details=https%3A%2F%2Fwww.thehowles.love%2F&location=Grace%20Episcopal%20Church%2C%20St.%20Francisville%2C%20Louisiana&dates=20260516T180000/20260516T220000&ctz=America/Chicago"
       className="inline-flex items-center justify-center rounded-2xl border border-[#E7D9BF] bg-[#FFF8EC] px-6 py-3 text-[#6B725E] font-medium shadow transition-transform hover:-translate-y-0.5 hover:shadow-lg focus:outline-none focus-visible:ring-2 focus-visible:ring-[#CAA55A] focus-visible:ring-offset-2"
     >
       Add to Calendar

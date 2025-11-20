d2a5e0ea97ea9618da042d6ee4feb4ea8e609532
Hayden Howle <hayden.howle@teambespin.us>
2025-11-19 19:11:45 -0600

    fixed add calender button

--- components/Buttons/AddCalender.tsx
+++ components/Buttons/AddCalender.tsx
@@ -3,7 +3,7 @@
 export default function AddCalenderButton() {
   return (
     <a
-      href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MDVwOGhrY3BybmtkdWRsbWVoa2M4dGg0YWUgaGF5ZGVuLmhvd2xlQHRlYW1iZXNwaW4udXM&tmsrc=hayden.howle%40teambespin.us"
+      href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NTdhOWZndnJvZDg4NTQ0NGJkOGc3b3FhajcgaG93bGVyNDY5NUBt&tmsrc=howler4695%40gmail.com"
       className="inline-flex items-center justify-center rounded-2xl border border-[#E7D9BF] bg-[#FFF8EC] px-6 py-3 text-[#6B725E] font-medium shadow transition-transform hover:-translate-y-0.5 hover:shadow-lg focus:outline-none focus-visible:ring-2 focus-visible:ring-[#CAA55A] focus-visible:ring-offset-2"
     >
       Add to Calendar

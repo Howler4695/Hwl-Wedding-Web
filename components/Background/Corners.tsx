b1b1adaba5ce38e70da1d54763a689b1eeed0d67
Hayden Howle <hayden.howle@teambespin.us>
2025-11-21 16:08:17 -0600

    lower priority on corners

--- components/Background/Corners.tsx
+++ components/Background/Corners.tsx
@@ -5,25 +5,31 @@ export const MagCorners = () => (
   <>
     <div
       aria-hidden
-      className="pointer-events-none select-none fixed -top-20 -left-20 h-72 w-72 rotate-[140deg] opacity-70 z-0"
+      className="pointer-events-none select-none fixed -top-20 -left-20 h-72 w-72 rotate-[140deg] opacity-60 z-0"
     >
       <Image
         src={MagNoStem}
         alt=""
         fill
         sizes="288px"
-        className="object-contain"
+        loading="lazy"
+        fetchPriority="low"
+        placeholder="empty"
+        className="object-contain "
       />
     </div>
     <div
       aria-hidden
-      className="pointer-events-none select-none fixed -bottom-24 -right-24 h-72 w-72 rotate-[320deg] opacity-70 z-0"
+      className="pointer-events-none select-none fixed -bottom-24 -right-24 h-72 w-72 rotate-[320deg] opacity-60 z-0"
     >
       <Image
         src={MagNoStem}
         alt=""
         fill
         sizes="288px"
+        loading="lazy"
+        fetchPriority="low"
+        placeholder="empty"
         className="object-contain"
       />
     </div>

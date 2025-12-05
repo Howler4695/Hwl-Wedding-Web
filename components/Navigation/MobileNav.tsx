77b879f6813beb10c4783d878f80f429a74576d8
Hayden Howle <hayden.howle@teambespin.us>
2025-12-05 13:16:03 -0600

    finished mobile menu refactor

--- components/Navigation/MobileNav.tsx
+++ components/Navigation/MobileNav.tsx
@@ -1,45 +1,69 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLinks } from "./NavLinks";
 
 export const MobileNavButton = ({ edit }: { edit: boolean }) => {
   const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [open]);
+
   return (
     <>
-      <button
-        className="bg-hannah-pink w-14 h-14 rounded-full flex flex-col gap-1 items-center justify-center shadow-2xl border border-pink-200"
-        onClick={() => setOpen(!open)}
-      >
-        <span
-          className={`hamburger-bar ${
-            open ? "rotate-45 translate-y-[6px]" : ""
-          }`}
-        />
-        <span
-          className={`hamburger-bar ${open ? "opacity-0" : "opacity-100"}`}
-        />
-        <span
-          className={`hamburger-bar ${
-            open ? "-rotate-45 -translate-y-[6px]" : ""
-          }`}
-        />
-      </button>
+      <div className="bg-hannah-pink rounded-full">
+        <button
+          className={`bg-pink-500/40 w-14 h-14 rounded-full flex flex-col gap-1 items-center justify-center shadow-2xl border border-gray-600
+          transition-all duration-300 z-70`}
+          onClick={() => setOpen(!open)}
+        >
+          <span
+            className={`hamburger-bar ${
+              open ? "rotate-45 translate-y-[6px]" : ""
+            }`}
+          />
+          <span
+            className={`hamburger-bar ${open ? "opacity-0" : "opacity-100"}`}
+          />
+          <span
+            className={`hamburger-bar ${
+              open ? "-rotate-45 -translate-y-[6px]" : ""
+            }`}
+          />
+        </button>
+      </div>
 
       <div
         className={`
-    fixed inset-0 z-50 h-screen overflow-y-auto bg-pink-400/50 backdrop-blur-xl
-    transform transition-transform duration-300 ease-in-out
-    ${open ? "translate-y-0" : "-translate-y-full"}
-    sm:hidden 
-  `}
+          fixed inset-0 z-40 md:hidden 
+          bg-pink-500/40 backdrop-blur-xl w-screen
+          transition-[clip-path] duration-500 ease-out
+          ${
+            open
+              ? "[clip-path:circle(150%_at_calc(100%_-_2.75rem)_2.75rem)]"
+              : "[clip-path:circle(0%_at_calc(100%_-_2.75rem)_2.75rem)]"
+          }
+        `}
       >
-        <div className="font-heading font-bold text-xl sm:text-2xl text-white px-4 pt-7">
-          Hannah & Hayden&apos;s Wedding
+        <div
+          className={`
+            fixed inset-0 z-50 h-screen overflow-y-auto 
+            transition-opacity duration-300 delay-150
+            ${open ? "opacity-100" : "opacity-0"}
+            sm:hidden 
+          `}
+        >
+          <div className="font-heading font-bold text-xl sm:text-2xl text-white px-4 pt-7">
+            Hannah & Hayden&apos;s Wedding
+          </div>
+          <nav className="mt-20 flex flex-col items-center gap-6 text-xl">
+            <NavLinks edit={edit} onNav={() => setOpen(!open)} />
+          </nav>
         </div>
-        <nav className="mt-20 flex flex-col items-center gap-6 text-xl">
-          <NavLinks edit={edit} onNav={() => setOpen(!open)} />
-        </nav>
       </div>
     </>
   );
